cfde1a8e07f8631c302884884457a75d007a7be8
riyapss <rpuri@unbc.ca>
2025-12-07 20:33:52 -0800

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { TodoComponent } from './app/todo/todo.component';
+import { appConfig } from './app/app.config';
 
-bootstrapApplication(TodoComponent)
+bootstrapApplication(TodoComponent, appConfig)
   .catch(err => console.error(err));
