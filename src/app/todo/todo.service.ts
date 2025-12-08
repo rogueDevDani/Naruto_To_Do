1f6938c6a15b1ca543bd00b19126a89b0d0ddb78
riyapss <rpuri@unbc.ca>
2025-12-07 23:05:22 -0800

    Created todo.service.ts

---
+++ src/app/todo/todo.service.ts
@@ -0,0 +1,42 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TodoService {
+
+  private apiUrl = 'http://localhost:3000';
+
+  constructor(private http: HttpClient) {}
+
+  // ---------------- Tasks ----------------
+  getTasks(): Observable<any[]> {
+    return this.http.get<any[]>(`${this.apiUrl}/tasks`);
+  }
+
+  createTask(task: any): Observable<any> {
+    return this.http.post(`${this.apiUrl}/tasks`, task);
+  }
+
+  updateTask(id: number, task: any): Observable<any> {
+    return this.http.put(`${this.apiUrl}/tasks/${id}`, task);
+  }
+
+  deleteTask(id: number): Observable<any> {
+    return this.http.delete(`${this.apiUrl}/tasks/${id}`);
+  }
+
+  // ---------------- GameState ----------------
+  getGameState(): Observable<any> {
+    return this.http.get(`${this.apiUrl}/gamestate`);
+  }
+
+  updateGameState(state: any): Observable<any> {
+    return this.http.post(`${this.apiUrl}/gamestate`, state);
+  }
+
+}
+
+
